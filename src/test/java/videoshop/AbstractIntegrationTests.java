2f490936fdcb17585f959db1742a883eca0d1578
Andreas Zaschka <andreas.zaschka@tu-dresden.de>
2015-12-02 16:46:35 +0100

    #50 - Fix with @Transaction annotation, should fix the problem.

--- src/test/java/videoshop/AbstractIntegrationTests.java
+++ src/test/java/videoshop/AbstractIntegrationTests.java
@@ -15,6 +15,8 @@
  */
 package videoshop;
 
+import javax.transaction.Transactional;
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -26,4 +28,5 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = VideoShop.class)
+@Transactional
 public abstract class AbstractIntegrationTests {}
