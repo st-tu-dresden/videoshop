61bb3939152a83f9c9996495d9c3d4221d574c06
Daniel <technikamateur@gmx.de>
2018-11-24 12:40:24 +0100

    removed BindingResult import

--- src/main/java/videoshop/customer/CustomerController.java
+++ src/main/java/videoshop/customer/CustomerController.java
@@ -21,7 +21,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.Assert;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
