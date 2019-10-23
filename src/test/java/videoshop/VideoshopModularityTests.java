6c7b430d6aa4e38c8149787039bbb7f54c8196d2
Oliver Drotbohm <odrotbohm@pivotal.io>
2019-10-23 12:49:27 +0200

    #120 - Added Modulith component diagrams.
    VideoshopModularityTests now contains a test method to create an overall component diagram as well as a module centric one for each of the business modules contained in Videoshop. They can be found in target/generated-docs/moduliths.
    
    Upgrade to Modulith 1.0 RC3, as it exposes new options for coloring and display name customizations.

--- src/test/java/videoshop/VideoshopModularityTests.java
+++ src/test/java/videoshop/VideoshopModularityTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 the original author or authors.
+ * Copyright 2018-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,19 +15,68 @@
  */
 package videoshop;
 
+import de.olivergierke.modulith.docs.Documenter;
+import de.olivergierke.modulith.docs.Documenter.Options;
+import de.olivergierke.moduliths.model.Module;
 import de.olivergierke.moduliths.model.Modules;
 
+import java.io.IOException;
+import java.util.Optional;
+import java.util.function.Predicate;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.util.StringUtils;
 
 /**
  * Unit test verifying project modularity.
- * 
+ *
  * @author Oliver Gierke
  */
+@TestInstance(Lifecycle.PER_CLASS)
 class VideoshopModularityTests {
+
+	Modules modules = Modules.of(VideoShop.class);
+	Predicate<Module> isSalespointModule = it -> it.getBasePackage().getName().startsWith("org.salespoint");
 
 	@Test
 	void assertModularity() {
-		Modules.of(VideoShop.class).verify();
+		modules.verify();
+	}
+
+	@Test // #120
+	void writeComponentDiagrams() throws IOException {
+
+		Options options = Options.defaults() //
+				.withColorSelector(this::getColorForModule) //
+				.withDefaultDisplayName(this::getModuleDisplayName) //
+				.withTargetOnly(isSalespointModule);
+
+		Documenter documenter = new Documenter(modules);
+		documenter.writeModulesAsPlantUml(options);
+
+		modules.stream().filter(isSalespointModule.negate()) //
+				.forEach(it -> documenter.writeModuleAsPlantUml(it, options));
+	}
+
+	private Optional<String> getColorForModule(Module module) {
+
+		String packageName = module.getBasePackage().getName();
+
+		if (packageName.startsWith("org.salespoint")) {
+			return Optional.of("#ddddff");
+		} else if (packageName.startsWith("videoshop")) {
+			return Optional.of("#ddffdd");
+		} else {
+			return Optional.empty();
+		}
+	}
+
+	private String getModuleDisplayName(Module module) {
+
+		return module.getBasePackage().getName().startsWith("videoshop") //
+				? "Videoshop :: ".concat(StringUtils.capitalize(module.getDisplayName())) //
+				: module.getDisplayName();
 	}
 }
