c9058742104bcd8ea9621205eb433df706d49f87
Oliver Drotbohm <odrotbohm@vmware.com>
2021-10-26 16:10:44 +0200

    Fix name of order package for tests.

--- src/test/java/videoshop/order/WebSecurityIntegrationTests.java
+++ src/test/java/videoshop/order/WebSecurityIntegrationTests.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package videoshop.orders;
+package videoshop.order;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
