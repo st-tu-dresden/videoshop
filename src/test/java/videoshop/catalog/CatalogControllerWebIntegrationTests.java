7fda3c78b08b89c39d2d4e7d5efc5cd4eea2b3b2
Oliver Drotbohm <info@odrotbohm.de>
2021-12-21 23:44:42 +0100

    #155 - Polishing.
    Formatting and copyright years update.

--- src/test/java/videoshop/catalog/CatalogControllerWebIntegrationTests.java
+++ src/test/java/videoshop/catalog/CatalogControllerWebIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2018 the original author or authors.
+ * Copyright 2013-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,9 +44,9 @@ class CatalogControllerWebIntegrationTests {
 	@Test
 	void sampleMvcIntegrationTest() throws Exception {
 
-		mvc.perform(get("/blurays")). //
-				andExpect(status().isOk()).//
-				andExpect(model().attributeExists("catalog")).//
-				andExpect(model().attribute("catalog", is(not(emptyIterable()))));
+		mvc.perform(get("/blurays")) //
+				.andExpect(status().isOk()) //
+				.andExpect(model().attributeExists("catalog")) //
+				.andExpect(model().attribute("catalog", is(not(emptyIterable()))));
 	}
 }
